064fe0665fb5c0cdd5bd80f6c38a5cb9265c25ba
mkubibaev <mkubibaev@gmail.com>
2019-01-11 02:42:22 +0600

    added reset game function

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const Cell = props => {
+	const cellClasses = ['Cell'];
+
+	if (props.clicked) {
+		cellClasses.push('clicked');
+	}
+
 	return (
-		<div className="Cell">
+		<div
+			className={cellClasses.join(' ')}
+			onClick={props.onClick}
+		>
 			{props.hasItem ? <span>O</span> : null}
 		</div>
 	);
--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -2,41 +2,57 @@ import React, {Component} from 'react';
 import Cell from "../../components/Cell/Cell";
 
 class Board extends Component {
-	constructor(props) {
-		super(props);
 
-		this.state = {
-			cells: []
-		};
+	state = {
+		cells: [],
+		tries: 0
+	};
 
+
+	componentDidMount() {
+		this.resetGame();
+	}
+
+	onClick = id => {
+		const cells = this.state.cells;
+
+		if (!cells[id].clicked) {
+			cells[id].clicked = true;
+
+			this.setState({cells, tries: this.state.tries + 1})
+		}
+	};
+
+	resetGame = () => {
+		const cells = [];
 		const randomNum = Math.floor(Math.random() * 36);
 		console.log(randomNum);
 
 		for (let i = 0; i < 36; i++) {
+			const cell = {id: i, clicked: false};
 
-			if (i === randomNum) {
-				this.state.cells.push({id: i, hasItem: true});
-			} else {
-				this.state.cells.push({id: i, hasItem: false});
-			}
-
-
+			cell.hasItem = i === randomNum;
+			cells.push(cell);
 		}
 
-	}
-
-
-
+		this.setState({cells, tries: 0})
+	};
 
 	render () {
 		return (
 			<div className="Board">
+				<div className="BoardInner">
 				{this.state.cells.map(cell => (
 					<Cell
 						key={cell.id}
+						clicked={cell.clicked}
 						hasItem={cell.hasItem}
+						onClick={() => this.onClick(cell.id)}
 					/>
 				))}
+				</div>
+				<p>Tries: {this.state.tries}</p>
+				<button onClick={this.resetGame}>Reset</button>
 			</div>
 		)
 	}
